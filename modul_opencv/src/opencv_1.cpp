97141a5c76e93782b5f27ae17764bb08203e4ccc
Wild3005 <wildanrisqi04@gmail.com>
2024-08-30 15:07:21 +0700

    modulku

--- modul_opencv/src/opencv_1.cpp
+++ modul_opencv/src/opencv_1.cpp
@@ -4,7 +4,7 @@
 int main(){
     // mengupload gambar
     cv::Mat src = cv::imread("/home/ichbinwil/folder_wildan/Modul-Oprec/modul_opencv/rubik.png");
-    cv::Mat src_resize,gray_img, hsv_img;
+    cv::Mat src_resize,gray_img, hsv_img,bright_img,dark_img;
     cv::resize(src,src_resize,cv::Size(240,240));
 
     // convert warna ke gray pada gambar
@@ -15,6 +15,10 @@ int main(){
 
     // crop image
     cv::Mat crop_img = src_resize(cv::Range(80,240), cv::Range(150,240));
+
+    //membuat gambar terang dan gelap
+    src_resize.convertTo(bright_img,-1,1,50);// membuat terang
+    src_resize.convertTo(dark_img,-1,1,-50);// membuat gelap
 
     // informasi foto
     std::cout << "width:" << src_resize.size().width << std::endl;
@@ -33,6 +37,11 @@ int main(){
     cv::imshow("resize_img",cv::WINDOW_AUTOSIZE);
     cv::imshow("gray_image",gray_img);
     cv::imshow("hsv_image",hsv_img);
+    cv::waitKey(0);
+    cv::namedWindow("bright_img",cv::WINDOW_AUTOSIZE);
+    cv::imshow("bright_img",bright_img);
+    cv::namedWindow("dark_img",cv::WINDOW_AUTOSIZE);
+    cv::imshow("dark_img",dark_img);
     cv::waitKey(0);
 
     return 0;
